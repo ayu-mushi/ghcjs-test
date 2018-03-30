0bf070ace301c94c845ad1a5dd4304f79fa871b5
ayu-mushi <ayu.mushi2015@gmail.com>
2018-03-30 18:45:35 +0900

    Add new message to gambling. Test using local storage. Hold merginal profits between helper functions.

--- clicker/src/Lib.hs
+++ clicker/src/Lib.hs
@@ -1,5 +1,3 @@
--- TODO 通知 title "(1002)->(2005) Clicker"
-
 {-# LANGUAGE JavaScriptFFI, InterruptibleFFI #-}
 {-# LANGUAGE OverloadedStrings  #-}
 {-# LANGUAGE ScopedTypeVariables  #-}
@@ -13,9 +11,9 @@ import Reflex.Dom as Reflex
   holdDyn, holdUniqDyn, EventName(Click), domEvent, foldDyn, mapDyn, El, tickLossy, TickInfo(_tickInfo_lastUTC, _tickInfo_n, _tickInfo_alreadyElapsed),  delay, count,  ffilter, FunctorMaybe(fmapMaybe), keypress, display, leftmost, button, simpleList, webSocket, webSocketConfig_send,
   RawWebSocket(..), tag, current, setValue, value, textInputConfig_initialValue, mconcatDyn, combineDyn, attachPromptlyDynWith, zipDynWith, constant,
   Reflex, updated, gate, DomBuilder, splitE,
-  MonadHold, hold, tagPromptlyDyn, textArea, textArea_value, TextArea, attributes, constDyn, (=:), textAreaConfig_initialValue, attach, attachWith, getPostBuild, PostBuild, attachWidget)
+  MonadHold, hold, tagPromptlyDyn, textArea, textArea_value, TextArea, attributes, constDyn, (=:), textAreaConfig_initialValue, attach, attachWith, getPostBuild, PostBuild, attachWidget, askJSContext, performEvent, ffor, PerformEvent, Performable)
 import Reflex.Dom.Main (Widget, mainWidgetWithHead, mainWidgetWithHead')
-import Reflex.Class (accum, Dynamic, Event, Behavior)
+import Reflex.Class (accum, Dynamic, Event, Behavior, headE)
 import Reflex.Spider (SpiderTimeline, Global, Spider)
 import qualified Data.Text as Text(pack, lines, unlines)
 import Data.Time.Clock (getCurrentTime, UTCTime, diffUTCTime, NominalDiffTime)
@@ -27,7 +25,7 @@ import Control.Monad (void, join)
 import Control.Monad.Writer (WriterT, tell, runWriterT, execWriterT)
 import Data.Functor ((<$), ($>))
 import Control.Monad.Trans (lift)
-import Data.Monoid ()
+import Data.Monoid (Sum(Sum, getSum))
 import Data.Map as Map (singleton)
 import Control.Lens (Lens', (&), (.~))
 import Control.Lens.Iso(iso, Iso')
@@ -35,8 +33,11 @@ import Data.Semigroup (Semigroup, (<>))
 import GHCJS.DOM.Document(getHeadUnsafe)
 import GHCJS.DOM (currentWindowUnchecked, currentDocumentUnchecked)
 import GHCJS.DOM.Types (toElement)
+import GHCJS.DOM.Window (getLocalStorage)
+import GHCJS.DOM.Storage (setItem, getItem)
 import Foreign.JavaScript.TH(JSContextSingleton(..))
 import Reflex.TriggerEvent.Class(TriggerEvent)
+--import LocalStorage (getLocal, saveLocal)
 
 buttonDyn :: (DomBuilder t m, MonadWidget t m) => Dynamic t Text -> m (Event t ())
 buttonDyn t = do
@@ -67,13 +68,13 @@ type Ability = Int -> NominalDiffTime -> Price
 type Amount t = Dynamic t Int -- クッキー、おばあさん、工場などの数を表すDynamicだが、値段などは含まない
 -- 値段もクッキーだけど。
 
-work :: (MonadWidget t m) => Amount t -> Ability -> NominalDiffTime -> m (Dynamic t Int)
+work :: (MonadWidget t m) => Amount t -> Ability -> NominalDiffTime -> m (Dynamic t Int, Event t Int)
 work labor ability interval = do
   ct <- liftIO getCurrentTime
   (tick::Event t TickInfo) <- tickLossy interval ct
   let (product::Event t Int) = attachPromptlyDynWith (\n t -> ability n $ _tickInfo_alreadyElapsed t) labor tick
   totalproduct <- foldDyn (+) 0 product
-  return totalproduct
+  return (totalproduct, product)
 
 buy :: (Reflex t) => Dynamic t Price -> Amount t -> Event t () -> Event t ()
 buy priceDyn cookie buying =
@@ -85,14 +86,18 @@ buyDyn priceDyn cookie buyEv = do
   commodity <- count $ buy priceDyn cookie buyEv
   return commodity
 
-consum :: (MonadWidget t m) => Dynamic t Price -> Amount t -> m (Dynamic t Price)
-consum priceDyn commodity = foldDyn (+) 0 $ fmap (* (-1)) $ tag (current priceDyn) $ updated commodity
+consum :: (MonadWidget t m) => Dynamic t Price -> Amount t -> m (Event t Price)
+consum priceDyn commodity = accum (+) 0 $ fmap (* (-1)) $ tag (current priceDyn) $ updated commodity
 
-profit :: (MonadWidget t m) => Amount t -> Ability -> NominalDiffTime -> Dynamic t Price -> m (Dynamic t Int) -- profit = 利潤 = benefit - cost
+consum1 :: (Reflex t) => Dynamic t Price -> Amount t -> Event t Price
+consum1 priceDyn commodity = fmap (* (-1)) $ tag (current priceDyn) $ updated commodity
+
+
+profit :: (MonadWidget t m) => Amount t -> Ability -> NominalDiffTime -> Dynamic t Price -> m (Event t Int) -- profit = 利潤 = benefit - cost
 profit labor ability interval priceDyn = do
-  benefit <- work labor ability interval
-  cost <- consum priceDyn labor
-  return $ zipDynWith (+) cost benefit
+  (benefit, mbenefit) <- work labor ability interval
+  let mcost = consum1 priceDyn labor
+  return $ fmap getSum $ (Sum <$> mcost) <> (Sum <$> mbenefit)
 
 data CommoditySpec = CommoditySpec
   { priceSeq :: PriceSeq
@@ -121,7 +126,8 @@ grandma cookie = mdo
   grandma_price' <- delay 2 $ fmap (\n -> floor $ (20.0::Float) * ((1.15::Float) ^ n)) $ updated $ uniq_grandma
   grandma_price <- holdDyn 20 grandma_price'
   (uniq_grandma :: Amount t) <- buyDyn grandma_price cookie grandma_button
-  profit_grandma <- profit uniq_grandma (const :: Int -> NominalDiffTime -> Int) 1 grandma_price
+  mprofit_grandma <- profit uniq_grandma (const :: Int -> NominalDiffTime -> Int) 1 grandma_price
+  profit_grandma <- foldDyn (+) 0 mprofit_grandma
   return profit_grandma
 
 factory :: (MonadWidget t m) => Amount t -> m (Dynamic t Price)
@@ -129,10 +135,11 @@ factory cookie = mdo
   factory_button <- getRet $ workerView "factory" factory_price uniq_factory profit_factory
   let factory_price = fmap (\n -> floor $ (50.0::Float) * ((1.15::Float) ^ n)) uniq_factory
   (uniq_factory :: Amount t) <- buyDyn factory_price cookie factory_button
-  profit_factory <- profit uniq_factory (\n _ -> 10 * n) 3 factory_price
+  mprofit_factory <- profit uniq_factory (\n _ -> 10 * n) 3 factory_price
+  profit_factory <- foldDyn (+) 0 mprofit_factory
   return profit_factory
 
-data Dice = Gold | Silver | Copper -- 出目
+data Dice = Gold | Silver | Copper deriving (Eq)-- 出目
 
 gamblingResultStr :: Dice -> Text
 gamblingResultStr Gold = "You got Gold!(10% possibility)\n"
@@ -166,14 +173,24 @@ gambling cookie = do
     let marginal_benefit = fmap (uncurry gamblePointing) $ attach (current gambling_price) results
 
     (gambling_benefit :: Dynamic t Int) <- foldDyn (+) 0 marginal_benefit
-    gambling_cost <- consum gambling_price uniq_gambling
+    gambling_cost <- foldDyn (+) 0 $ consum1 gambling_price uniq_gambling
 
-    let profit_gambling = (+) <$> gambling_benefit <*> gambling_cost
+    let profit_gambling = zipDynWith (+) gambling_benefit gambling_cost
+
     return (marginal_benefit, gambling_price_original, profit_gambling, results)
+
 
   let gamble_name (x::Int) | x == 10 = "You got Gold!(10% possibility)\n"
                        | x < 10 && 6 < x = "You got Silver(40% possibility)\n"
                        | otherwise = "You got Copper(50% possibility)\n"
+
+-- "「銀」を三回獲得――スキル【強運】を開放します。"
+  firstDame <- headE $ gate (current $ fmap ((-50) >=) profit_gambling) ("[破滅Lv1を開放しました]あなたは50以上のクッキーをギャンブルで失いました……賭け金はやればやるほど増えるから、もっとやれば取り返せるかも！がんばれ＞＜\n [Unlock the Vice named 'Ruin Lv.1'] ――You lost 50 cookie by gambling.\n" <$ (updated cookie))
+  tell firstDame
+
+  (numOfGold :: Dynamic t Int)<- lift $ count $ ffilter (==Gold) results
+  luckyman <- headE $ gate (fmap (==3) $ current $ numOfGold) ("「金」を三回獲得――スキル【強運】を開放します。\n You got three golds ――thus, unlock a skill named 'Lucky'\n" <$ (updated cookie))
+  tell luckyman
 
   tell $ fmap gamblingResultStr results
 
@@ -183,17 +200,18 @@ gambling cookie = do
   return profit_gambling
 
 -- Dynamic t [a] -> [Dynamic t a]
-{- debt :: (MonadWidget t m) => Amount t -> m (Dynamic t Price)
-debt cookie = do
-  debt_price <- getRet $ el' "div" $ mdo
-    let debt_price = fmap (\n -> floor $ (-20.0::Float) * ((1.15::Float) ^ n)) uniq_debt -- 借金
-    debt_button <- lift $ getRet $ workerView "借金" debt_price uniq_debt uniq_debt -- buttonDyn で返済ボタン
-    (uniq_debt :: Amount t) <- lift $ buyDyn debt_price cookie debt_button
-    (priceHistory::Dynamic t [Int]) <- lift $ accum (\x y -> y:x) [] $ updated debt_price
-    (paybackButtons::Dynamic t [(El t, Int)]) <- lift $ simpleList priceHistory $
-       \m -> el' "button" $ display $ fmap (Text.pack . ("debt: "++) . show . (/1.15)) $ m
-    return debt_price
-  accum (+) 0 (updated debt_price)-}
+-- switch
+--debt :: (MonadWidget t m) => Amount t -> m (Dynamic t Price)
+--debt cookie = do
+--  debt_price <- getRet $ el' "div" $ mdo
+--    let debt_price = fmap (\n -> floor $ (-20.0::Float) * ((1.15::Float) ^ n)) uniq_debt -- 借金
+--    debt_button <- lift $ getRet $ workerView "借金" debt_price uniq_debt uniq_debt -- buttonDyn で返済ボタン
+--    (uniq_debt :: Amount t) <- lift $ buyDyn debt_price cookie debt_button
+--    (priceHistory::Dynamic t [Int]) <- lift $ accum (\x y -> y:x) [] $ updated debt_price
+--    (paybackButtons::Dynamic t [(El t, Int)]) <- lift $ simpleList priceHistory $
+--       \m -> el' "button" $ display $ fmap (Text.pack . ("debt: "++) . show . (/1.15)) $ m
+--    return debt_price
+--  accum (+) 0 (updated debt_price)
 -- end </workers>
 
 -- 徐々に出てくるようにする。いきなり多額の借金は負えない
@@ -203,10 +221,11 @@ debt cookie = do
 
 makeCookie :: (MonadWidget t m) => [Dynamic t Price] -> m (Amount t)
 makeCookie sums = getRet $ el' "div" $ do
-  cookie_click <- button "get cookie"
+  cookie_click <- button "Mani wheel"
   clicked <- count cookie_click
   let cookie = foldl1 (zipDynWith (+)) $ clicked:sums
   display cookie
+  el "div" $ dynText $ fmap (("manual: "<>). Text.pack . show) clicked
   return cookie
 
 timer :: (MonadWidget t m) => m (Dynamic t Integer)
@@ -217,12 +236,6 @@ timer = do
     dynText $ fmap (\time -> Text.pack $ "総プレイ時間:" <> show time <> "s") tick
   return tick
 
-textArea_lines :: Reflex t => Lens' (TextArea t) (Dynamic t [Text])
-textArea_lines = textArea_value . liner
-  where
-    liner :: Reflex t => Iso' (Dynamic t Text) (Dynamic t [Text])
-    liner = iso (fmap Text.lines) (fmap Text.unlines)
-
 -- 値段 = 初期値と値上げ率から定まる等比数列
 data PriceSeq = PriceSeq {
   initialPrice :: Price,
@@ -231,14 +244,17 @@ data PriceSeq = PriceSeq {
 
 headWidget :: (MonadWidget t m) => Amount t -> m ()
 headWidget cookie = do
-  el "title" $ dynText $ fmap (Text.pack . show) cookie
+  el "title" $ dynText $ fmap toTitle cookie
   return ()
 
+  where toTitle cookie = "(" <> (Text.pack $ show cookie) <> ") Clicker"
+
 -- CpSの計算
-myWidget :: (MonadWidget t m) => m (Amount t)
+myWidget :: (MonadWidget t m, PerformEvent t m) => m (Amount t)
 myWidget = do
   -- ダブルクリック判定と同様の、連続で買われたかどうかの判定をすると良さそう
   -- ダブルクリック判定は、普通にクリック数をカウントして、一定時間経ったらリセットする
+
   wallTime <- timer
 
   (cookie, log) <- runWriterT $ mdo
@@ -258,6 +274,21 @@ myWidget = do
     & attributes .~ constDyn ("readonly" =: "readonly" <> "style" =: "width: 500px; height: 200px;")
     & setValue .~ logAcc
 
+  localstorage <- liftIO $ do
+    window <- currentWindowUnchecked
+    localstorage <- getLocalStorage window
+    return localstorage
+
+  saved <- button "save"
+  performEvent $ ffor (tagPromptlyDyn cookie saved) $ \cookie -> liftIO $ do
+    setItem localstorage ("saveLocation"::String) $ Text.pack $ show cookie
+
+  recovered <- button "recover"
+  savedata <- performEvent $ ffor recovered $ \() -> liftIO $ do
+    Just tx <- getItem localstorage ("saveLocation"::String)
+    return tx
+
+  dynText =<< (holdDyn "" savedata)
   -- 投資、資本
   -- 借金、ギャンブル、リスク
   -- 破産
@@ -267,7 +298,6 @@ myWidget = do
   -- 実績解放
   -- TODO: MVCの分離
   -- 運をお金で買う
-  -- "「銀」を三回獲得――スキル【強運】を開放します。"
   -- 曜日ごとに儲かる
   -- 値上げ時間をちょっとあとにしたら? 連打まとめ買いでお得
   return cookie
@@ -281,4 +311,10 @@ myWidget = do
 
 
 someFunc :: IO ()
-someFunc = mainWidgetWithHead' (headWidget, (\()->myWidget))
+someFunc = mainWidgetWithHead' (headWidget, const myWidget)
+
+-- 経過時間と生産力からクッキーの量を導出
+-- 設備の量が途中で変わる場合はどうする
+-- save button
+-- DONE> TODO 通知 title "(1002)->(2005) Clicker"
+-- 型クラスのデフォルトを使って挙動の共通性と例外を管理
