3dbf0c6f837498fe5a281bd01a9ce15c0d879403
ayu-mushi <ayu.mushi2015@gmail.com>
2018-03-30 09:16:57 +0900

    Use mainWidgetWithHead' in order to view the number of cookies in the title element.

--- clicker/src/Lib.hs
+++ clicker/src/Lib.hs
@@ -1,7 +1,9 @@
+-- TODO 通知 title "(1002)->(2005) Clicker"
+
 {-# LANGUAGE JavaScriptFFI, InterruptibleFFI #-}
 {-# LANGUAGE OverloadedStrings  #-}
 {-# LANGUAGE ScopedTypeVariables  #-}
-{-# LANGUAGE RecursiveDo  #-}
+{-# LANGUAGE RecursiveDo, Rank2Types, FlexibleContexts#-}
 module Lib
     ( someFunc
     ) where
@@ -11,8 +13,10 @@ import Reflex.Dom as Reflex
   holdDyn, holdUniqDyn, EventName(Click), domEvent, foldDyn, mapDyn, El, tickLossy, TickInfo(_tickInfo_lastUTC, _tickInfo_n, _tickInfo_alreadyElapsed),  delay, count,  ffilter, FunctorMaybe(fmapMaybe), keypress, display, leftmost, button, simpleList, webSocket, webSocketConfig_send,
   RawWebSocket(..), tag, current, setValue, value, textInputConfig_initialValue, mconcatDyn, combineDyn, attachPromptlyDynWith, zipDynWith, constant,
   Reflex, updated, gate, DomBuilder, splitE,
-  MonadHold, hold, tagPromptlyDyn, textArea, textArea_value, TextArea, attributes, constDyn, (=:), textAreaConfig_initialValue, attach, attachWith, getPostBuild)
+  MonadHold, hold, tagPromptlyDyn, textArea, textArea_value, TextArea, attributes, constDyn, (=:), textAreaConfig_initialValue, attach, attachWith, getPostBuild, PostBuild, attachWidget)
+import Reflex.Dom.Main (Widget, mainWidgetWithHead, mainWidgetWithHead')
 import Reflex.Class (accum, Dynamic, Event, Behavior)
+import Reflex.Spider (SpiderTimeline, Global, Spider)
 import qualified Data.Text as Text(pack, lines, unlines)
 import Data.Time.Clock (getCurrentTime, UTCTime, diffUTCTime, NominalDiffTime)
 import Data.Text.Internal (Text)
@@ -20,7 +24,7 @@ import Control.Monad.IO.Class (liftIO)
 import System.Random (randomR, mkStdGen, Random, newStdGen)
 import Control.Monad.Fix (MonadFix)
 import Control.Monad (void, join)
-import Control.Monad.Writer (WriterT, tell, runWriterT)
+import Control.Monad.Writer (WriterT, tell, runWriterT, execWriterT)
 import Data.Functor ((<$), ($>))
 import Control.Monad.Trans (lift)
 import Data.Monoid ()
@@ -28,11 +32,35 @@ import Data.Map as Map (singleton)
 import Control.Lens (Lens', (&), (.~))
 import Control.Lens.Iso(iso, Iso')
 import Data.Semigroup (Semigroup, (<>))
+import GHCJS.DOM.Document(getHeadUnsafe)
+import GHCJS.DOM (currentWindowUnchecked, currentDocumentUnchecked)
+import GHCJS.DOM.Types (toElement)
+import Foreign.JavaScript.TH(JSContextSingleton(..))
+import Reflex.TriggerEvent.Class(TriggerEvent)
 
 buttonDyn :: (DomBuilder t m, MonadWidget t m) => Dynamic t Text -> m (Event t ())
 buttonDyn t = do
   (e, _) <- el' "button" $ dynText t
   return $ domEvent Click e
+
+buttonDyn' :: (DomBuilder t m, MonadWidget t m) => m a -> m (Event t (), a)
+buttonDyn' m = do
+  (e, a) <- el' "button" $ m
+  return (domEvent Click e, a)
+
+-- https://hackage.haskell.org/package/reflex-gloss-scene-0.1.2/docs/src/Reflex-Gloss-Random.html#foldRand
+foldGen :: (Reflex t, MonadHold t m, MonadFix m) => s -> (s -> (a, s)) -> Event t () -> m (Event t a)
+foldGen initialState f input = do
+  rec
+    curState <- hold initialState newState
+    let (output, newState) = splitE $ f <$> tag curState input
+  return output
+
+foldRandomRs :: (MonadWidget t m, Reflex t, MonadHold t m, MonadFix m, Random a) => (a, a) -> Event t () -> m (Event t a)
+foldRandomRs range ev = do
+  gen <- liftIO newStdGen
+  e <- foldGen gen (randomR range) ev
+  return e
 
 type Price = Int
 type Ability = Int -> NominalDiffTime -> Price
@@ -74,11 +102,12 @@ data CommoditySpec = CommoditySpec
 -- CpS
 
 -- View Side in Model and View
-workerView :: (DomBuilder t m, MonadWidget t m) => String -> Dynamic t Int -> Dynamic t Price -> Dynamic t Price -> m (El t, Event t ())
+workerView :: (DomBuilder t m, MonadWidget t m) => Text -> Dynamic t Int -> Dynamic t Price -> Dynamic t Price -> m (El t, Event t ())
 workerView name p uniq profit = el' "div" $ do
-  button <- buttonDyn $ fmap (\price -> Text.pack ("buy " ++ name ++ " ($"++ show price ++" cookies)")) p
-  dynText $ fmap (\n -> Text.pack $ "数:" ++ show n ++ "匹います！") uniq
-  dynText $ fmap (\prof -> Text.pack $ "利潤: " ++ show prof ++ "だよ☆") profit
+  text name
+  button <- buttonDyn $ fmap (\price -> "buy " <> name <> " ($"<> (Text.pack $ show price) <>" cookies)") p
+  dynText $ fmap (\n -> Text.pack $ "数:" <> show n <> "匹います！") uniq
+  dynText $ fmap (\prof -> Text.pack $ "総利潤: " <> show prof <> "だよ☆") profit
   return button
 
 getRet :: Functor f => f (a, b) -> f b
@@ -103,47 +132,89 @@ factory cookie = mdo
   profit_factory <- profit uniq_factory (\n _ -> 10 * n) 3 factory_price
   return profit_factory
 
+data Dice = Gold | Silver | Copper -- 出目
+
+gamblingResultStr :: Dice -> Text
+gamblingResultStr Gold = "You got Gold!(10% possibility)\n"
+gamblingResultStr Silver = "You got Silver(40% possibility)\n"
+gamblingResultStr Copper = "You got Copper(50% possibility)\n"
+
+gamblePointing :: Price -> Dice -> Int
+gamblePointing priceNow Gold = floor $ ((fromInteger $ toInteger priceNow)::Float) * 2.5
+gamblePointing priceNow Silver = floor $ ((fromInteger $ toInteger priceNow)::Float) * 1.5
+gamblePointing priceNow Copper = floor $ ((fromInteger $ toInteger priceNow)::Float) * 0.5
+
+gambleSeq :: (MonadWidget t m) => Event t () -> m (Event t Dice)
+gambleSeq ev = do
+  (randEv :: Event t Int) <- foldRandomRs (0, 10) ev
+  return $ fmap gamble_sheet randEv
+
+  where gamble_sheet x | x == 10 = Gold
+                       | x < 10 && 6 < x = Silver
+                       | otherwise = Copper
+
 gambling :: (MonadWidget t m) => Amount t -> WriterT (Event t Text) m (Dynamic t Price) -- ギャンブル
 gambling cookie = do
-  (marginal_benefit, gambling_price_original, profit_gambling) <- lift $ mdo
+  (marginal_benefit, gambling_price_original, profit_gambling, results) <- lift $ mdo
     let gambling_price = fmap (\n -> floor $ (20.0::Float) * ((1.15::Float) ^ n)) $ uniq_gambling
     let gambling_price_original = gambling_price
     gambling_button <- getRet $ workerView "gambling" gambling_price uniq_gambling profit_gambling
     (uniq_gambling :: Amount t) <- buyDyn gambling_price cookie gambling_button
-    (randEv :: Event t Int) <- foldRandomRs (0, 10) (updated $ void $ uniq_gambling)
+    (results :: Event t Dice) <- gambleSeq $ updated $ void uniq_gambling
 
-    let gamble_sheet (priceNow::Price) (x::Int) | x == 10 = floor $ ((fromInteger $ toInteger priceNow)::Float) * 2.5
-                       | x < 10 && 6 < x = priceNow * 2
-                       | otherwise = floor $ ((fromInteger $ toInteger priceNow)::Float) * 0.5
-
-    let marginal_benefit = fmap (uncurry gamble_sheet) $ attach (current gambling_price) randEv
+    -- 限界クッキー
+    let marginal_benefit = fmap (uncurry gamblePointing) $ attach (current gambling_price) results
 
     (gambling_benefit :: Dynamic t Int) <- foldDyn (+) 0 marginal_benefit
     gambling_cost <- consum gambling_price uniq_gambling
 
     let profit_gambling = (+) <$> gambling_benefit <*> gambling_cost
-    return (marginal_benefit, gambling_price_original, profit_gambling)
+    return (marginal_benefit, gambling_price_original, profit_gambling, results)
+
+  let gamble_name (x::Int) | x == 10 = "You got Gold!(10% possibility)\n"
+                       | x < 10 && 6 < x = "You got Silver(40% possibility)\n"
+                       | otherwise = "You got Copper(50% possibility)\n"
+
+  tell $ fmap gamblingResultStr results
 
   let gambling_log = attachWith (\price mb -> Text.pack $ "you get " <> show mb <> " cookies by gambling!(original cookies is: " <> show price <> "cookies)\n") (current gambling_price_original) marginal_benefit
   tell gambling_log
+
   return profit_gambling
 
+-- Dynamic t [a] -> [Dynamic t a]
+{- debt :: (MonadWidget t m) => Amount t -> m (Dynamic t Price)
+debt cookie = do
+  debt_price <- getRet $ el' "div" $ mdo
+    let debt_price = fmap (\n -> floor $ (-20.0::Float) * ((1.15::Float) ^ n)) uniq_debt -- 借金
+    debt_button <- lift $ getRet $ workerView "借金" debt_price uniq_debt uniq_debt -- buttonDyn で返済ボタン
+    (uniq_debt :: Amount t) <- lift $ buyDyn debt_price cookie debt_button
+    (priceHistory::Dynamic t [Int]) <- lift $ accum (\x y -> y:x) [] $ updated debt_price
+    (paybackButtons::Dynamic t [(El t, Int)]) <- lift $ simpleList priceHistory $
+       \m -> el' "button" $ display $ fmap (Text.pack . ("debt: "++) . show . (/1.15)) $ m
+    return debt_price
+  accum (+) 0 (updated debt_price)-}
 -- end </workers>
 
+-- 徐々に出てくるようにする。いきなり多額の借金は負えない
+-- 借金、返せるか返せないかギリギリのところを狙う。マイナスになったら破産して死。
+-- 前方報酬、後方苦痛、依存性
+-- Trap -- 最初だけ快楽がある
 
 makeCookie :: (MonadWidget t m) => [Dynamic t Price] -> m (Amount t)
 makeCookie sums = getRet $ el' "div" $ do
   cookie_click <- button "get cookie"
-  (cookie :: Amount t) <- count cookie_click >>= \sumcookie -> return $ foldl1 (zipDynWith (+)) $ sumcookie:sums
+  clicked <- count cookie_click
+  let cookie = foldl1 (zipDynWith (+)) $ clicked:sums
   display cookie
-  -- (holdDyn 0 $ tagPromptlyDyn cookie (updated tick)) >>= display -- 1秒ごとにdisplayを更新
   return cookie
 
 timer :: (MonadWidget t m) => m (Dynamic t Integer)
 timer = do
   ct <- liftIO getCurrentTime
   (tick::Dynamic t Integer) <- (fmap (fmap _tickInfo_n) $ tickLossy 1 ct) >>= holdDyn 0
-  dynText $ fmap (\time -> Text.pack $ "プレイ総時間:" <> show time) tick
+  el "div" $
+    dynText $ fmap (\time -> Text.pack $ "総プレイ時間:" <> show time <> "s") tick
   return tick
 
 textArea_lines :: Reflex t => Lens' (TextArea t) (Dynamic t [Text])
@@ -158,25 +229,29 @@ data PriceSeq = PriceSeq {
   markupPercent :: Float
   }
 
-myWidget :: (MonadWidget t m) => m ()
+headWidget :: (MonadWidget t m) => Amount t -> m ()
+headWidget cookie = do
+  el "title" $ dynText $ fmap (Text.pack . show) cookie
+  return ()
+
+-- CpSの計算
+myWidget :: (MonadWidget t m) => m (Amount t)
 myWidget = do
-  text "Grandma は 買ってから値上げまで2秒かかるのですばやく高速で買い上げると得!"
   -- ダブルクリック判定と同様の、連続で買われたかどうかの判定をすると良さそう
+  -- ダブルクリック判定は、普通にクリック数をカウントして、一定時間経ったらリセットする
   wallTime <- timer
 
-  rec
-    cookie <- makeCookie [profit_grandma, profit_factory, profit_gambling]
-    profit_grandma <- grandma cookie
-    profit_factory <- factory cookie
-    (profit_gambling, log) <- runWriterT $ do
-      (onload :: Event t ()) <- lift getPostBuild
-      tell ("Welcome to Clicker.\n" <$ onload)
-      profit_gambling <- gambling cookie
-      return profit_gambling
+  (cookie, log) <- runWriterT $ mdo
+    (onload :: Event t ()) <- lift getPostBuild
+    tell ("Welcome to Clicker.\n" <$ onload)
+    tell ("Grandma は 買ってから値上げまで2秒かかるのですばやく高速で買い上げると得!\n" <$ onload)
 
-    let debt_price = fmap (\n -> floor $ (-20.0::Float) * ((1.15::Float) ^ n)) uniq_debt -- 借金
-    debt_button <- getRet $ workerView "借金" debt_price uniq_debt uniq_debt -- buttonDyn で返済ボタン
-    (uniq_debt :: Amount t) <- buyDyn debt_price cookie debt_button
+    cookie <- lift $ makeCookie [profit_grandma, profit_factory, profit_gambling]
+    profit_grandma <- lift $ grandma cookie
+    profit_factory <- lift $ factory cookie
+    profit_gambling <- gambling cookie
+
+    return cookie
 
   (logAcc::Event t Text) <- accum (<>) "" log
   console <- textArea $ def
@@ -195,21 +270,15 @@ myWidget = do
   -- "「銀」を三回獲得――スキル【強運】を開放します。"
   -- 曜日ごとに儲かる
   -- 値上げ時間をちょっとあとにしたら? 連打まとめ買いでお得
-  return ()
+  return cookie
 
--- https://hackage.haskell.org/package/reflex-gloss-scene-0.1.2/docs/src/Reflex-Gloss-Random.html#foldRand
-foldGen :: (Reflex t, MonadHold t m, MonadFix m) => s -> (s -> (a, s)) -> Event t () -> m (Event t a)
-foldGen initialState f input = do
-  rec
-    curState <- hold initialState newState
-    let (output, newState) = splitE $ f <$> tag curState input
-  return output
+-- クッキーじゃなくて徳にすることも考えられる
+-- マニ車を回す的な
+-- 私度商 ～ Private Tokudo Campany
+-- 徳の単位ってなんだ。離散量なのか？1菩薩2菩薩がいいのでは。あるいは刹那
 
-foldRandomRs :: (MonadWidget t m, Reflex t, MonadHold t m, MonadFix m, Random a) => (a, a) -> Event t () -> m (Event t a)
-foldRandomRs range ev = do
-  gen <- liftIO newStdGen
-  e <- foldGen gen (randomR range) ev
-  return e
+-- titleDyn :: MonadWidget t m => Dynamic t Text -> m ()
+
 
 someFunc :: IO ()
-someFunc = mainWidget myWidget
+someFunc = mainWidgetWithHead' (headWidget, (\()->myWidget))
